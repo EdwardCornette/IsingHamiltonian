f70fde2fe733bd288523e5506d275d3be8f7571a
Edward Cornette <eddiecornette2@gmail.com>
2024-04-30 16:31:14 -0400

    added docstrings to bitstring class

--- molecool/bitstring.py
+++ molecool/bitstring.py
@@ -8,10 +8,16 @@ class BitString:
     """
 
     def __init__(self, N):  # costructor
+        """Constructor
+
+        Parameters
+        ----------
+        N      : int
+            number of bits
+
+        """
         self.N = N
-        self.config = np.zeros(
-            N, dtype=int
-        )  # creates an int array of length n, with default values of 0
+        self.config = np.zeros(N, dtype=int)  # creates an int array of length n, with default values of 0
 
     def __repr__(self):  # returns a string representing the object
         temp = ""
@@ -34,6 +40,14 @@ class BitString:
         return self.N
 
     def on(self):  # number of on bits
+        """returns the number of on bits
+
+        Returns
+        -------
+        counter  : int
+            number of on bits
+        
+        """
         counter = 0
         for i in range(0, self.N):
             if self.config[i] == 1:
@@ -48,12 +62,29 @@ class BitString:
         return counter
 
     def flip_site(self, i):  # toggles bit at given location
+
+        """toggles the value of a given bit
+
+        Parameters
+        ----------
+        i : int
+            index of bit
+
+        """
         if self.config[i] == 1:
             self.config[i] = 0
         else:
             self.config[i] = 1
 
     def int(self):  # returns base ten value of bitstring
+        """returns decimal value of bitstring
+
+        Returns
+        -------
+        total  : int
+            the value of the bitstring
+        
+        """
         total = 0
         for i in range(0, self.N):
             total = total + (2**i * self.config[self.N - 1 - i])
@@ -61,13 +92,30 @@ class BitString:
         return total
 
     def set_config(self, s: list[int]):  # creates bitstring from list
+        """sets the config of a bitstring from a list
+
+        Parameters
+        ----------
+        s      : list[int]
+            the bitstring configuration
+
+        """
         for i in range(0, self.N):
             if s[i] == 0:
+
                 self.config[i] = 0
             else:
                 self.config[i] = 1
 
     def set_int_config(self, dec: int):  # creates bitstring from base 10 value
+        """sets the config of a bitstring from a decimal value
+
+        Parameters
+        ----------
+        dec      : int
+            decimal value
+
+        """
         val = dec
         temp = np.zeros(self.N, dtype=int)
         counter = 0
